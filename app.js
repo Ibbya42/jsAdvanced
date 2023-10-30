16ebd7c3380cb3524b21454b30aa8163330d4019
ibbya42 <ibbyali4214@gmail.com>
2023-10-30 22:27:43 +1300

    30/10 Final Push

--- app.js
+++ app.js
@@ -3,26 +3,35 @@
 // Question 1
 // Show Rating 
 
-function showRating(rating){
-    // Initialize a ratings variable 
-    let ratings = "";
-    // Loop through the iteration and return a star for each iteration 
-    // Use Math.floor to round off the number 
-    for (let i = 0 ; i < Math.floor(rating); ++i){
-        ratings = ratings + "*"
-        // Remove space from the last iteration 
-        if (i !== Math.floor(rating)){
-            ratings = ratings + " "
-        }
-    }
-    // check if there are decimals in the value.
-    // If the number is not an integar add a "."
-    if (!Number.isInteger(rating)){
-        ratings = ratings + ".";
-    }
+// function showRating(rating){
+//     // Initialize a ratings variable 
+//     let ratings = "";
+//     // Loop through the iteration and return a star for each iteration 
+//     // Use Math.floor to round off the number 
+//     for (let i = 0 ; i < Math.floor(rating); ++i){
+//         ratings = ratings + "*"
+//         // Remove space from the last iteration 
+//         if (i !== Math.floor(rating)){
+//             ratings = ratings + " "
+//         }
+//     }
+//     // check if there are decimals in the value.
+//     // If the number is not an integar add a "."
+//     if (!Number.isInteger(rating)){
+//         ratings = ratings + ".";
+//     }
 
-    return ratings
+//     return ratings
+// }
+
+// console.log(showRating(4.5))
+// console.log(showRating(3))
+
+// Question 2
+// Low to High
+
+function sortLowToHigh(numbers){
+    return numbers.sort((a, b) => a - b);
 }
 
-console.log(showRating(4.5))
-console.log(showRating(3))
+console.log(sortLowToHigh([20,100,50,70]))
